4262475f51a415ff7d5b78df6b54a8f4b853c1b2
Blake Knight <bknight@gsandf.com>
2021-11-11 08:50:36 -0600

    :art: use simplified ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,9 @@
 module.exports = {
-  extends: [
-    require.resolve('amper-scripts/config/eslint'),
-    'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended'
-  ],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  extends: [require.resolve('amper-scripts/config/eslint')],
   rules: {
-    '@typescript-eslint/explicit-function-return-type': ['off'],
-    '@typescript-eslint/no-explicit-any': ['off']
+    '@typescript-eslint/ban-ts-comment': [
+      'error',
+      { 'ts-expect-error': 'allow-with-description' }
+    ]
   }
 };
