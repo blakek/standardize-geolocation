d9d7144354855efd877f769c93a9214609ad5051
Blake Knight <bknight@gsandf.com>
2021-11-11 09:09:06 -0600

    :green_heart: remove redundant rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,3 @@
 module.exports = {
-  extends: [require.resolve('amper-scripts/config/eslint')],
-  rules: {
-    '@typescript-eslint/ban-ts-comment': [
-      'error',
-      { 'ts-expect-error': 'allow-with-description' }
-    ]
-  }
+  extends: [require.resolve('amper-scripts/config/eslint')]
 };
